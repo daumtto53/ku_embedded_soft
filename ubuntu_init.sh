0f6f248222633d870c77576295b5ce3c8d6dd9f0
daumtto53 <daumtto53@gmail.com>
2022-03-13 14:28:29 +0900

    ADDED: ubuntu_init.sh

---
+++ ubuntu_init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get install build-essential linux-headers-$(uname -r)
+
+sudo rm /var/lib/apt/lists/lock; sudo rm /var/cache/apt/archives/lock; sudo rm /var/lib/dpkg/lock*;
+
+sudo dpkg --configure -a; sudo apt update;
+
+sudo apt-get install build-essential linux-headers-$(uname -r);
+
+sudo apt-get install libelf-dev
+
+sudo apt-get install util-linux
+
+sudo apt-get install git
+
+sudo apt-get install 
+
+
