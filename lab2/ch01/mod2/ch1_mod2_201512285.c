b9edfcdb2a73a09a925d3ea34cca156d48e16d41
daumtto53 <daumtto53@gmail.com>
2022-03-13 16:41:40 +0900

    MODIFIED : mod2.c's Hello,Bye

--- lab2/ch01/mod2/ch1_mod2_201512285.c
+++ lab2/ch01/mod2/ch1_mod2_201512285.c
@@ -6,7 +6,6 @@ extern int	set_my_id(int id);
 
 static int __init	ch1_mod2_init(void)
 {
-	printk(KERN_NOTICE "Hello MOD2\n");
 	printk(KERN_NOTICE "My Id : %d\n", get_my_id());
 	set_my_id(201512285);
 	printk(KERN_NOTICE "My Id : %d\n", get_my_id());
@@ -15,7 +14,6 @@ static int __init	ch1_mod2_init(void)
 
 static void __exit	ch1_mod2_exit(void)
 {
-	printk(KERN_NOTICE "Bye MOD2\n");
 }
 
 module_init(ch1_mod2_init);
