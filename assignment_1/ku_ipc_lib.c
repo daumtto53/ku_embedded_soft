ef38a23251efc969ccf6d257d7df5b27141b7611
daumtto53 <daumtto53@gmail.com>
2022-05-01 12:40:56 +0900

    UPDATED : for report ku_ipc_lib.c

--- assignment_1/ku_ipc_lib.c
+++ assignment_1/ku_ipc_lib.c
@@ -47,16 +47,14 @@ int ku_msgget(int key, int msgflg)
 {
 	int		dev;
 	int		msgq_id;
-	struct	msgq_metadata	msgq_meta; //change var name
+	struct	msgq_metadata	msgq_meta;
 
 	dev = open("/dev/ku_ipc_dev", O_RDWR);
-	printf("fd : [%d]\n", dev);
 
 	msgq_meta.msqid = key;
 	msgq_meta.msgflg = msgflg;
 
 	msgq_id = ioctl(dev, KU_IPC_MSGGET, &msgq_meta);
-	printf("ku_msgget : print ret_val : [%d]\n", msgq_id);
 	close(dev);
 	return (msgq_id);
 }
@@ -65,15 +63,13 @@ int ku_msgclose(int msqid)
 {
 	int		dev;
 	int		msgclose_ret;
-	struct	msgq_metadata	msgq_meta; //change var name
+	struct	msgq_metadata	msgq_meta; 
 
 	dev = open("/dev/ku_ipc_dev", O_RDWR);
-	printf("fd : [%d]\n", dev);
 
 	msgq_meta.msqid = msqid;
 
 	msgclose_ret = ioctl(dev, KU_IPC_MSGCLOSE, &msgq_meta);
-	printf("ku_msgclose : print ret_val : [%d]\n", msgclose_ret);
 	close(dev);
 	return (msgclose_ret);
 }
@@ -82,7 +78,7 @@ int ku_msgsnd(int msqid, void *msgp, int msgsz, int msgflg)
 {
 	int dev;
 	int msgsnd_ret;
-	struct	msgq_metadata	msgq_meta; //change var name
+	struct	msgq_metadata	msgq_meta; 
 
 	dev = open("/dev/ku_ipc_dev", O_RDWR);
 
@@ -92,7 +88,6 @@ int ku_msgsnd(int msqid, void *msgp, int msgsz, int msgflg)
 	msgq_meta.msgflg = msgflg;
 
 	msgsnd_ret = ioctl(dev, KU_IPC_MSGSND, &msgq_meta);
-	printf("ku_msgsnd : print ret_val : [%d]\n", msgsnd_ret);
 	close(dev);
 	return (msgsnd_ret);
 }
@@ -101,7 +96,7 @@ int	ku_msgrcv(int msqid, void *msgp, int msgsz, long msgtyp, int msgflg)
 {
 	int dev;
 	int msgrcv_ret;
-	struct	msgq_metadata	msgq_meta; //change var name
+	struct	msgq_metadata	msgq_meta; 
 
 	msgq_meta.msqid = msqid;
 	msgq_meta.msgp = msgp;
@@ -111,82 +106,7 @@ int	ku_msgrcv(int msqid, void *msgp, int msgsz, long msgtyp, int msgflg)
 
 	dev = open("/dev/ku_ipc_dev", O_RDWR);
 	msgrcv_ret = ioctl(dev, KU_IPC_MSGRCV, &msgq_meta);
-	printf("ku_msgrcv : print ret_val : [%d]\n", msgrcv_ret);
-	//rcv한 msgp 출력하기
 	close(dev);
 	return (msgrcv_ret);
 }
 
-//delete
-int main()
-{
-	int i;
-	int ret_val[MAX_ENTRY];
-	char	tmp[50][MSG_LEN];
-
-	struct ku_msgbuf msgbuf[KUIPC_MAXMSG + 30];
-	struct ku_msgbuf *alloc_buf;
-	
-	//msg_get TESTCASE
-	for (i = 0; i < MAX_ENTRY; i++)
-	{
-		ret_val[i] = ku_msgget(i, KU_IPC_CREAT);
-		printf("pid : [%d]\n", getpid());
-		printf("ret_val : [%d]\n", ret_val[i]);
-	}
-
-
-	for (i = 0; i < MAX_ENTRY; i++)
-	{
-		ret_val[i] = ku_msgget(i, KU_IPC_CREAT);
-		printf("pid : [%d]\n", getpid());
-		printf("ret_val : [%d]\n", ret_val[i]);
-	}
-	
-	//msg_close TESTCASE
-//	for (i = 0; i < MAX_ENTRY; i++)
-//	{
-//		ret_val[i] = ku_msgclose(i);
-//		printf("pid : [%d]\n", getpid());
-//		printf("ret_val : [%d]\n", ret_val[i]);
-//	}
-//      
-//	for (i = 0; i < MAX_ENTRY; i++)
-//	{
-//		ret_val[i] = ku_msgclose(i);
-//		printf("pid : [%d]\n", getpid());
-//		printf("ret_val : [%d]\n", ret_val[i]);
-//	}
-
-	//msg_snd TESTCASE
-	{
-		printf("\n@@@ MSGSND TESTCASE\n");
-
-		for (i = 0; i < KUIPC_MAXMSG + 10; i++)
-		{
-			msgbuf[i].type = 20 - i;
-			sprintf(tmp[i], "MSGSND : type:[%d]", 20-i); 
-			memcpy(&msgbuf[i].text, tmp[i], 128);
-			ret_val[0] = ku_msgsnd(0, msgbuf + i, 128, KU_IPC_NOWAIT);
-			printf("MSGSND : case:[%d], ret_value:[%d]\n", i, ret_val[0]);
-		}
-	}	
-
-	//msg_rcv TESTCASE
-	{
-		printf("\n@@@ MSGRCV TESTCASE\n");
-
-		for (i = 0; i < KUIPC_MAXMSG; i++)
-		{
-			ret_val[0] = ku_msgrcv(0, &msgbuf[i], 128, i + 1, KU_IPC_NOWAIT);
-			printf("MSGRCV : case:[%d], mtext:[%s], ret_value:[%d]\n", i, msgbuf[i].text, ret_val[0]);
-		}
-
-		ret_val[0] = ku_msgrcv(0, &msgbuf[0], 128, 0, KU_IPC_NOWAIT);
-		printf("MSGRCV : case:[%d], mtext:[%s], ret_value:[%d]\n", 21, msgbuf[0].text, ret_val[0]);
-		
-
-	}
-
-	return (0);
-}
